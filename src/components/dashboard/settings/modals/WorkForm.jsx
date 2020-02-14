3d68f5437aeaaf64be2122fea8c80de308066c16
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-02-14 16:01:20 +0300

    Add url validation

--- src/components/dashboard/settings/modals/WorkForm.jsx
+++ src/components/dashboard/settings/modals/WorkForm.jsx
@@ -9,12 +9,15 @@ import Success from '../../../core/Success';
 import DateTimePicker from '../../../core/DateTimePicker';
 import Button from '../../../core/Button';
 import Input from '../../../core/Input';
+import { validURL } from '../../../utils/validation';
 
 export default class WorkForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      work: props.work || {}
+      work: props.work || {},
+      projectLinkError: false,
+      repoLinkError: false
     };
   }
 
@@ -39,6 +42,17 @@ export default class WorkForm extends React.Component {
 
   onSave = e => {
     e.preventDefault();
+    const {
+      work: { repository_link, project_link }
+    } = this.state;
+    if (project_link) {
+      let result = validURL(project_link);
+      return this.setState({ projectLinkError: !result });
+    }
+    if (repository_link) {
+      let result = validURL(repository_link);
+      return this.setState({ repoLinkError: !result });
+    }
     const { proceed } = this.props;
     if (proceed) {
       proceed(this.state.work);
@@ -50,6 +64,7 @@ export default class WorkForm extends React.Component {
     const { errors } = this.props,
       start_date = `${this.state.work.start_year}-${this.state.work.start_month}`,
       end_date = `${this.state.work.end_year}-${this.state.work.end_month}`;
+    const { projectLinkError, repoLinkError } = this.state;
 
     return (
       <div>
@@ -111,12 +126,14 @@ export default class WorkForm extends React.Component {
               </FormGroup>
             </div>
             <div className="col-sm-6">
+              {projectLinkError && <FieldError message="Please enter valid Url" />}
               <FormGroup>
                 <label className="control-label">Project Link</label>
                 <Input onChange={this.onInputChange.bind(this, 'project_link')} value={this.state.work.project_link} />
               </FormGroup>
             </div>
             <div className="col-sm-6">
+              {repoLinkError && <FieldError message="Please enter valid Url" />}
               <FormGroup>
                 <label className="control-label">Repository Link</label>
                 <Input onChange={this.onInputChange.bind(this, 'repository_link')} value={this.state.work.repository_link} />
