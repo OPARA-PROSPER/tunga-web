d1d9514bacf3875820f5f95bcfc3009f3e315e3b
Katuula Joel <joel@tunga.io>
2020-03-23 09:53:09 +0300

    footer fix on landing page

--- src/latest-landing/layout/Footer/Footer.js
+++ src/latest-landing/layout/Footer/Footer.js
@@ -311,14 +311,14 @@ class Footer extends Component {
                     <Col md="3" sm="6">
                         <div className="latest-from-our-blog">
                             <h4>LATEST FROM OUR BLOG</h4>
-                            {/* {firstBlogs.map(
+                            {firstBlogs.map(
                                 (blog, i) => (
                                     <li key={i}>
                                         <a href={blog.url} target="_blank">
                                             {blog.title}
                                         </a>{" "}
                                     </li>
-                                ))} */}
+                                ))}
                         </div>
                     </Col>
                 </Row>
