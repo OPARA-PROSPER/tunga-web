4b62ad7e2f293c4355690d5f5712976e71fab705
Katuula Joel <joel@tunga.io>
2020-04-09 18:49:14 +0300

    npx react-codemod pure-component

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -4,29 +4,33 @@ import React from 'react';
 import {addEventListeners, BUTTON_EVENTS} from './utils/events';
 import {filterButtonProps} from "./utils/forms";
 
-export default class Button extends React.Component {
-    static defaultProps = {
-        type: 'button',
-        variant: 'primary',
-        block: false,
-    };
-
-    static propTypes = {
-        type: PropTypes.string,
-        className: PropTypes.string,
-        variant: PropTypes.string,
-        size: PropTypes.string,
-        block: PropTypes.bool,
-    };
+const Button = props => {
+    
 
     render() {
         return (
-            <button type={this.props.type}
-                    className={`btn ${this.props.variant?`btn-${this.props.variant}`:''} ${this.props.className || ''} ${this.props.block?'btn-block':''} ${this.props.size ?`btn-${this.props.size}`:''}`}
-                    {...filterButtonProps(this.props)}
-                    {...addEventListeners(BUTTON_EVENTS, this.props)}>
-                {this.props.children}
+            <button type={props.type}
+                    className={`btn ${props.variant?`btn-${props.variant}`:''} ${props.className || ''} ${props.block?'btn-block':''} ${props.size ?`btn-${props.size}`:''}`}
+                    {...filterButtonProps(props)}
+                    {...addEventListeners(BUTTON_EVENTS, props)}>
+                {props.children}
             </button>
         );
     }
 }
+
+Button.defaultProps = {
+    type: 'button',
+    variant: 'primary',
+    block: false,
+};
+
+Button.propTypes = {
+    type: PropTypes.string,
+    className: PropTypes.string,
+    variant: PropTypes.string,
+    size: PropTypes.string,
+    block: PropTypes.bool,
+};
+
+export default Button;
